f805a752f9858ef8f85649e2bea0d7a0c9fec54d
poi <Poi2333>
2019-06-30 21:00:50 +0800

    add options

---
+++ options.py
@@ -0,0 +1,9 @@
+class Option:
+    def __init__(self):
+        self.learning_rate = 1e-4
+        self.batch_size = 10
+        self.total_epoch = 10000
+        self.device_name = 'cuda'
+        self.k = 9
+        self.pointnum = 2000
+        self.filename = 'test0'
